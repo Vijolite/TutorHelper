aa369e6928d9ac12387878a92ec8436a19f59bda
Ija Saporenkova <ija.sha@gmail.com>
2025-10-26 20:56:35 +0000

    Delete test data

--- TutorHelper/Program.cs
+++ TutorHelper/Program.cs
@@ -1,6 +1,4 @@
-using System.Configuration;
 using TutorHelper.Forms;
-using TutorHelper.Helpers;
 
 namespace TutorHelper
 {
@@ -12,37 +10,6 @@ namespace TutorHelper
         [STAThread]
         static void Main()
         {
-            var replacements = new Dictionary<string, string>
-                        {
-                            { "{studentName}", "Anna" },
-                            { "{parentName}", "Annas mom" },
-                            { "{lessonName}", "new lesson" }
-
-                        };
-
-            string templatePath = ConfigurationManager.AppSettings["InvoiceTemplatePath"];
-            string templateFile = templatePath + "summaryTemplate.html";
-            string withReplacements = templatePath + "invoice.html";
-
-            HtmlTemplateHelper.ReplacePlaceholders(templateFile, withReplacements, replacements);
-
-            string outputFileName = $"html_doc";
-            string outputPath = @$"{templatePath}\{outputFileName}.docx";
-            string outputPathPdf = @$"{templatePath}\{outputFileName}.pdf";
-
-
-            HtmlToWordConverter.Convert(withReplacements, outputPath);
-            //WordToPdfConverter.Convert(outputPath, outputPathPdf);
-
-            //HtmlToPdfConverter.Convert(withReplacements, outputPathPdf);
-
-            //CreateWordDocFromTemplate(templateType, outputFolderPath, outputFileName, replacements);
-
-
-
-
-
-
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
